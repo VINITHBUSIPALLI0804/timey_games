c944759e5fe120517a789e7e544305328ffed704
vinith busipalli <akwvinith12549@gmail.com>
2024-10-30 21:53:50 +0530

    Update snakeandladder.py

--- snakeandladder.py
+++ snakeandladder.py
@@ -15,3 +15,7 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 BROWN = (139, 69, 19)
+
+# Set up the screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("||SNAKE AND LADDER||")
